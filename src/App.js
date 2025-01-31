8d4f85cb70b647bfa7e2d0fb5865499700abfcfa
expercoder6 <expercoder6@gmail.com>
2025-01-31 18:00:35 +0530

    project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+   <h1>
+      Hello World with styles and animations in React Native (https://www.positronx.io)
+   </h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/test.js
@@ -0,0 +1,33 @@
+const hello = () => "hello world"
+
+console.log(hello());
+
+const myArray = ['Apple', 'Banana', 'Orange'];
+const mylist = myArray.map((item) => {
+  console.log(item);
+});
+
+const vehicles = ['mustang', 'f-150' , 'expedition'];
+const [car , truck, suv] = vehicles;
+console.log("car is: "+car);
+console.log("truck is: "+ truck);
+console.log("suv is: " +suv);
+
+console.log("----------------------------------------------------------------");
+const object1 = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+};
+
+const object2 = {
+    
+    name: 'Jane',
+    age: 25,
+    city: 'Los Angeles'
+
+}; 
+
+
+const bothObjects = {...object2, ...object1};
+console.log(bothObjects);
